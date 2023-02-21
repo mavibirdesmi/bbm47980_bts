02ec697aee0ef652cdbee743fe4a065a00e5a6a5
mavibirdesmi <mavibirdesmi@gmail.com>
2023-02-21 21:58:21 +0300

    added multiclass transformations for bts dataset

--- brain_tumor_segmentation/bts_dataset.py
+++ brain_tumor_segmentation/bts_dataset.py
@@ -3,12 +3,13 @@ import os
 import json
 from typing import Dict
 
-from monai.transforms.transform import Transform
+from monai.transforms.transform import Transform, MapTransform
+from monai.config import KeysCollection
 from monai.utils.enums import TransformBackends
 
 import torch
 
-def generate_sample_paths (
+def generate_sample_paths_from_json (
     root_path : str
 ):
     config_path = os.path.join(
@@ -38,8 +39,43 @@ def generate_sample_paths (
     return sample_paths
 
 class ConvertToMultiChannelBasedOnBtsClasses (Transform):
+    """
+    Converts 3 dimensional label to 4 dimensional based on the Brain Tumor
+    Classification (BTS) dataset
+
+    label 1 is the brain (gray matter)
+    label 2 is the tumour
+    """
     backend = [TransformBackends.TORCH]
 
     def __call__ (self, img : torch.Tensor):
         # expected ndim is 3 
-        pass
+        assert img.ndim == 3, "Image is expected to be 3 dimensional"
+
+        result = [img == 1, img == 2]
+        return torch.stack(result, dim=0)
+
+
+class ConvertToMultiChannelBasedOnBtsClassesd (MapTransform):
+    """
+    Dictionary based wrapper of ConvertToMultiChannelBasedOnBtsClasses
+
+    Converts 3 dimensional label to 4 dimensional based on the Brain Tumor
+    Classification (BTS) dataset
+
+    label 1 is the brain (gray matter)
+    label 2 is the tumour
+    """
+
+    backend = ConvertToMultiChannelBasedOnBtsClasses.backend
+
+    def __init__(self, keys: KeysCollection, allow_missing_keys: bool = False):
+        super().__init__(keys, allow_missing_keys)
+        self.converter = ConvertToMultiChannelBasedOnBtsClasses()
+    
+    def __call__(self, data):
+        data_dict = dict(data)
+        for key in self.key_iterator(data_dict):
+            data_dict[key] = self.converter(data_dict[key])
+        
+        return data_dict

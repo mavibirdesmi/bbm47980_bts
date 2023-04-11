868cfc6db5338a1eec6a477448076c2b385c43db
badayvedat <badayvedat@gmail.com>
2023-04-11 22:50:34 +0300

    Simplify old code

--- bts/swinunetr/train-old.py
+++ bts/swinunetr/train-old.py
@@ -3,8 +3,6 @@ import tempfile
 from functools import partial
 from typing import Dict, Optional, Union
 
-import nrrd
-import numpy as np
 import torch
 from monai import data
 from monai.inferers import sliding_window_inference
@@ -12,21 +10,17 @@ from monai.losses import DiceLoss
 from monai.metrics import DiceMetric
 from monai.networks.nets import SwinUNETR
 from monai.transforms import Activations, AsDiscrete
-from monai.utils.enums import MetricReduction
 from torch.utils.data import DataLoader
 
 import wandb
 from bts.common import logutils, miscutils
 from bts.common.miscutils import DotConfig
-from bts.data.dataset import get_test_dataset, get_train_dataset
+from bts.data.dataset import get_train_dataset
 
 logger = logutils.get_logger(__name__)
 
 
 wandb.init(name="Old code")
-
-
-root_path = "../../data"
 
 
 batch_size = 2
@@ -53,31 +47,6 @@ root_dir = tempfile.mkdtemp() if directory is None else directory
 print(root_dir)
 
 
-class AverageMeter(object):
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = np.where(self.count > 0, self.sum / self.count, self.sum)
-
-
-def save_checkpoint(model, epoch, filename="model.pt", best_acc=0, dir_add=root_dir):
-    state_dict = model.state_dict()
-    save_dict = {"epoch": epoch, "best_acc": best_acc, "state_dict": state_dict}
-    filename = os.path.join(dir_add, filename)
-    torch.save(save_dict, filename)
-    print("Saving checkpoint", filename)
-
-
 device = "cuda"
 device = torch.device(device)
 
@@ -102,13 +71,6 @@ val_every = 250
 
 torch.backends.cudnn.benchmark = True
 
-
-dice_loss = DiceLoss(to_onehot_y=False, sigmoid=True)
-post_sigmoid = Activations(sigmoid=True)
-post_pred = AsDiscrete(argmax=False, threshold=0.5)
-dice_acc = DiceMetric(
-    include_background=True, reduction=MetricReduction.MEAN_BATCH, get_not_nans=True
-)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epochs)
@@ -302,45 +264,28 @@ hyperparams = miscutils.load_hyperparameters(
 )
 
 
-def trainer(
-    model,
-    train_loader,
-    val_loader,
-    optimizer,
-    loss_func,
-    acc_func,
-    scheduler,
-    model_inferer=None,
-    start_epoch=0,
-    post_sigmoid=None,
-    post_pred=None,
-):
-    val_acc_max = 0.0
-    dices_brain = []
-    dices_tumor = []
-    dices_avg = []
-    loss_epochs = []
-    trains_epoch = []
+start_epoch = 0
+
+
+def main():
+    dice_loss = DiceLoss(to_onehot_y=False, sigmoid=True)
 
     for epoch in range(start_epoch, max_epochs):
         train_history = train_epoch(
             model=model,
-            loader=train_loader,
+            loader=dataloader,
             optimizer=optimizer,
             epoch=epoch,
-            loss_function=loss_func,
+            loss_function=dice_loss,
         )
 
         logger.info(f'Mean Train Loss: {round(train_history["Mean Train Loss"], 2)}')
         wandb.log(train_history, step=epoch)
 
         if (epoch + 1) % val_every == 0 or epoch == 0:
-            loss_epochs.append(train_history["Mean Train Loss"])
-            trains_epoch.append(int(epoch))
-
             val_history = val_epoch(
                 model,
-                loader=val_loader,
+                loader=dataloader,
                 loss_function=dice_loss,
                 roi_size=hyperparams.ROI,
                 sw_batch_size=hyperparams.SW_BATCH_SIZE,
@@ -351,102 +296,15 @@ def trainer(
             )
 
             # val_loss = val_history["Mean Val Loss"]
-            val_brain_acc = val_history["Mean Val Brain Acc"]
-            val_tumor_acc = val_history["Mean Val Tumor Acc"]
-            val_mean_acc = val_history["Mean Val Tumor Acc"]
+            # val_brain_acc = val_history["Mean Val Brain Acc"]
+            # val_tumor_acc = val_history["Mean Val Tumor Acc"]
+            # val_mean_acc = val_history["Mean Val Tumor Acc"]
 
             wandb.log(val_history, step=epoch)
-
-            dices_brain.append(val_brain_acc)
-            dices_tumor.append(val_tumor_acc)
-            dices_avg.append(val_mean_acc)
-            if val_tumor_acc > val_acc_max:
-                print(
-                    "new best ({:.6f} --> {:.6f}). ".format(val_acc_max, val_tumor_acc)
-                )
-                val_acc_max = val_tumor_acc
-                save_checkpoint(
-                    model,
-                    epoch,
-                    best_acc=val_acc_max,
-                )
 
             wandb.log({"Learning Rate": scheduler.get_lr()[0]}, step=epoch)
             scheduler.step()
 
-    print("Training Finished !, Best Accuracy: ", val_acc_max)
-    return (
-        val_acc_max,
-        dices_brain,
-        dices_tumor,
-        dices_avg,
-        loss_epochs,
-        trains_epoch,
-    )
 
-
-start_epoch = 0
-
-(
-    val_acc_max,
-    dices_brain,
-    dices_tumor,
-    dices_avg,
-    loss_epochs,
-    trains_epoch,
-) = trainer(
-    model=model,
-    train_loader=dataloader,
-    val_loader=dataloader,
-    optimizer=optimizer,
-    loss_func=dice_loss,
-    acc_func=dice_acc,
-    scheduler=scheduler,
-    model_inferer=model_inferer,
-    start_epoch=start_epoch,
-    post_sigmoid=post_sigmoid,
-    post_pred=post_pred,
-)
-
-
-print(f"train completed, best average dice: {val_acc_max:.4f} ")
-
-
-test_ds = get_test_dataset("/home/vedatb/senior-project/data/btsed_dataset")
-
-test_loader = data.DataLoader(
-    test_ds,
-    batch_size=1,
-    shuffle=False,
-    num_workers=2,
-    pin_memory=True,
-)
-
-
-model.load_state_dict(torch.load(os.path.join(root_dir, "model.pt"))["state_dict"])
-model.to(device)
-model.eval()
-
-model_inferer_test = partial(
-    sliding_window_inference,
-    roi_size=[roi[0], roi[1], roi[2]],
-    sw_batch_size=1,
-    predictor=model,
-    overlap=0.6,
-)
-
-
-with torch.no_grad():
-    for idx, batch_data in enumerate(test_loader):
-        image = batch_data["img"].cuda()
-        prob = torch.sigmoid(model_inferer_test(image))
-        seg = prob[0].detach().cpu().numpy()
-        seg = (seg > 0.5).astype(np.int8)
-        seg_out = np.zeros((seg.shape[1], seg.shape[2], seg.shape[3]))
-        seg_out[seg[0] == 1] = 1
-        seg_out[seg[1] == 1] = 2
-
-        nrrd.write(
-            file=f"/home/vedatb/senior-project/bbm47980_bts/old-predictions/{idx}.nrrd",
-            data=seg_out,
-        )
+if __name__ == "__main__":
+    main()

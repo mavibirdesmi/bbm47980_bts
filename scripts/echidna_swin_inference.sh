7b62ca0aa3737d4c1f09614e6bde7f67269781fa
badayvedat <badayvedat@gmail.com>
2023-04-25 03:44:56 +0300

    Update server scripts

--- scripts/echidna_swin_inference.sh
+++ scripts/echidna_swin_inference.sh
@@ -4,9 +4,13 @@
 #SBATCH --nodes 1
 #SBATCH --output=/home/%u/batch_outputs/%j.out
 #SBATCH --error=/home/%u/batch_outputs/%j.error
-#SBATCH --gres gpu:v100:1
+#SBATCH --gres gpu:p5000:1
+
+set -e
 
 source "$(poetry env info --path)/bin/activate"
+
+export WANDB__SERVICE_WAIT=300
 
 srun python -m bts.swinunetr.inference \
     --data-dir /home/vedatb/senior-project/data/btsed_dataset \
--- scripts/echidna_swin_train.sh
+++ scripts/echidna_swin_train.sh
@@ -4,10 +4,19 @@
 #SBATCH --nodes 1
 #SBATCH --output=/home/%u/batch_outputs/%j.out
 #SBATCH --error=/home/%u/batch_outputs/%j.error
-#SBATCH --gres gpu:v100:2
+#SBATCH --gres gpu:v100:3
+
+set -e
 
 source "$(poetry env info --path)/bin/activate"
+
+export WANDB__SERVICE_WAIT=300
 
 srun python -m bts.swinunetr.train \
     --data-dir /home/vedatb/senior-project/data/btsed_dataset \
     --output model-checkpoints
+
+srun python -m bts.swinunetr.inference \
+    --data-dir /home/vedatb/senior-project/data/btsed_dataset \
+    --model model-checkpoints/model.pt \
+    --output-dir predictions
